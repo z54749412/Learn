a04d7371b012fed243bbe24d8b710507256e42d6
zhuangguanxue <zhuangguanxue@yunniao.me>
2018-04-18 17:21:27 +0800

    md haskell

---
+++ HASKELL/test01.hs
@@ -0,0 +1,4 @@
+double x = x*2
+doubleAll x = succ(if x > 100 then x else double x)
+
+it'world = "it is a world"
---
+++ HASKELL/test02.hs
@@ -0,0 +1,7 @@
+lucky :: (Integral a) => a -> String
+
+lucky 7 = "LUCKY NUMBER SEVEN!"
+
+lucky 8 = "呀呀呀呀"
+
+lucky x = "Sorry, you're out of luck, pal!"
