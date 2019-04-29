a35f4f3e6a314d803b99a8e62ec2b1c1d17b8617
z54749412 <zhuangguanxue@163.com>
2019-04-29 09:30:56 +0800

    读取文件

--- Node/nodeServer/fileTest/test1.js
+++ Node/nodeServer/fileTest/test1.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const path = require('path')
 
 const fileName = path.resolve(__dirname, 'data.txt')
+console.log(__dirname)
+
+// 读文件
 
 // fs.readFile(fileName, (err, data) => {
 //   if (err) {
@@ -11,12 +14,20 @@ const fileName = path.resolve(__dirname, 'data.txt')
 //   console.log(data.toString())
 // })
 
-const content = '这是新写入的内容\n'
-const opt = {
-  flag: 'a'  // 追加写入，覆盖用 'w'
-}
-fs.writeFile(fileName, content, opt, err => {
-  if (err) {
-    console.error(err)
-  }
+// 写文件
+
+// const content = '这是新写入的内容\n'
+// const opt = {
+//   flag: 'a'  // 追加写入，覆盖用 'w'
+// }
+// fs.writeFile(fileName, content, opt, err => {
+//   if (err) {
+//     console.error(err)
+//   }
+// })
+
+// 判断文件是否存在
+
+fs.exists(fileName, exist => {
+  console.log(exist)
 })
