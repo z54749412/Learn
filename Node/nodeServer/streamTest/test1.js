46d4d16d7bc737dfd89ab3494fedd2e2902c5287
z54749412 <zhuangguanxue@163.com>
2019-04-29 09:38:40 +0800

    nodeServer流式返回数据

--- Node/nodeServer/streamTest/test1.js
+++ Node/nodeServer/streamTest/test1.js
@@ -3,29 +3,38 @@
 // process.stdin.pipe(process.stdout)
 
 
-// const http = require('http')
-
-// const server = http.createServer((req, res) => {
-//   if (req.method === 'POST') {
-//     req.pipe(res)
-//   }
-// })
-// server.listen(8000)
+// 服务方式返回文件
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const fileName1 = path.resolve(__dirname, 'data.txt')
+const server = http.createServer((req, res) => {
+  res.writeHead(200,  {'Content-Type':  'text/html;  charset=utf-8'});
+  if (req.method === 'GET') {
+    const readStream = fs.createReadStream(fileName1)
+    readStream.pipe(res)
+  }
+})
+server.listen(8000)
 
 // 通过 stream 读取文件
 
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 
-const fileName1 = path.resolve(__dirname, 'data.txt')
-const fileName2 = path.resolve(__dirname, 'data-bak.txt')
+// const fileName1 = path.resolve(__dirname, 'data.txt')
+// const fileName2 = path.resolve(__dirname, 'data-bak.txt')
 
-const readStream = fs.createReadStream(fileName1)
+// const readStream = fs.createReadStream(fileName1)
 
-const writeStream = fs.createWriteStream(fileName2)
+// const writeStream = fs.createWriteStream(fileName2)
 
-readStream.pipe(writeStream)
+// readStream.pipe(writeStream)
 
-readStream.on('end', () => {
-  console.log('备份完成！')
-})
+// readStream.on('data', chunk => {
+//   console.log(chunk.toString())
+// })
+
+// readStream.on('end', () => {
+//   console.log('备份完成！')
+// })
