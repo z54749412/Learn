5d1ee74df2911581731008e68d5a8cdbe778b665
z54749412 <zhuangguanxue@163.com>
2019-05-10 16:06:24 +0800

    初始化express博客系统

---
+++ Node/nodeServer/blog-express/db/mysql.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql')
+const { MYSQL_CONF } = require('../config/db')
+// 创建链接对象
+const con = mysql.createConnection(MYSQL_CONF)
+
+// 开始链接
+con.connect()
+
+// sql 指令
+
+// const sql = 'select * from blogs'
+
+// const sql = `update users set realname='李一' where id=7;`
+
+// const sql = `insert into blogs (title, content, createtime, author) values ('李四博客2', '咋回事呢', 1555291080308, '李四');`
+
+// 统一执行 sql 的函数
+function exec (sql) {
+  const promise = new Promise((resolve, reject) => {
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(result)
+    })
+  })
+  return promise
+}
+
+module.exports = {
+  exec,
+  escape: mysql.escape
+}
