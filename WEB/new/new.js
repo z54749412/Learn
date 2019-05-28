d4042209eb3a92854404661cc1b95798a99640cb
z54749412 <zhuangguanxue@163.com>
2019-05-28 18:45:05 +0800

    手动实现new

--- WEB/new/new.js
+++ WEB/new/new.js
@@ -16,8 +16,23 @@ function Person(name, age) {
     //     age:18
     // };
 }
+function Person2(name, age) {
+  this.name = name || 'zs';
+  this.age = age || 19;
+  return {
+      name: 'ls',
+      age:18
+  };
+}
+Person2.prototype.say = function () {
+  console.log(this.name);
+}
+
 Person.prototype.say = function () {
     console.log(this.name);
 }
 
 var mm = new2(Person);
+var mm2 = new2(Person2);
+console.log(mm)
+console.log(mm2)
